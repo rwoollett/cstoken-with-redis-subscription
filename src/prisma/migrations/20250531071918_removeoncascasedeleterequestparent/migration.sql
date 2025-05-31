71d11a0993f27a4df9cb019973793fb9144e8511
Rodney Woollett <rodney.woollett@gmail.com>
2025-05-31 19:19:36 +1200

    requestparent table

---
+++ src/prisma/migrations/20250531071918_removeoncascasedeleterequestparent/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Client" DROP CONSTRAINT "Client_parentIp_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Client" ADD CONSTRAINT "Client_parentIp_fkey" FOREIGN KEY ("parentIp") REFERENCES "RequestParent"("clientIp") ON DELETE RESTRICT ON UPDATE CASCADE;
